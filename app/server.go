f9bc928c31209a4ae9d23c2ea8859f72b8feb0b0
luudvan94 <33990794+luudvan94@users.noreply.github.com>
2023-11-22 13:52:23 -0500

    add exprire

--- app/server.go
+++ app/server.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"strings"
 	"sync"
+	"time"
 
 	// Uncomment this block to pass the first stage
 	"net"
@@ -22,11 +23,16 @@ type Connection struct {
 
 type Server struct {
 	mu  sync.RWMutex
-	kvs map[string]resp.Value
+	kvs map[string]Value
+}
+
+type Value struct {
+	value  resp.Value
+	expire time.Time
 }
 
 func NewServer() *Server {
-	return &Server{kvs: make(map[string]resp.Value)}
+	return &Server{kvs: make(map[string]Value)}
 }
 
 func NewConnection(conn net.Conn) *Connection {
@@ -41,12 +47,39 @@ func (conn *Connection) Close() {
 	conn.base.Close()
 }
 
+func (server *Server) get(key string) (Value, bool) {
+	server.mu.RLock()
+	value, ok := server.kvs[key]
+	server.mu.RUnlock()
+	fmt.Printf("Key: %s Value: %s\n", key, value.value.String())
+
+	now := time.Now()
+	if ok && (!value.expire.IsZero() && value.expire.Before(now)) {
+		fmt.Printf("Expired: %s\n", value.expire.String())
+		delete(server.kvs, key)
+		return value, false
+	}
+
+	return value, ok
+}
+
+func (server *Server) set(args []resp.Value) {
+	newValue := Value{}
+	newValue.value = args[2]
+
+	if len(args) >= 5 && args[3].String() == "px" {
+		expiryAmount := args[4].Integer()
+		newValue.expire = time.Now().Add(time.Duration(expiryAmount) * time.Millisecond)
+		fmt.Printf("Expiration time: %s\n", newValue.expire.String())
+	}
+	server.mu.Lock()
+	server.kvs[args[1].String()] = newValue
+	server.mu.Unlock()
+	fmt.Printf("set %s with %s\n", args[1].String(), args[2].String())
+}
+
 func (server *Server) HandleConnection(conn *Connection) {
 	defer conn.Close()
-
-	// rd := resp.NewReader(conn)
-	// var buf bytes.Buffer
-	// wr := resp.NewWriter(&buf)
 
 	for {
 		v, _, _, err := conn.ReadMultiBulk()
@@ -72,12 +105,10 @@ func (server *Server) HandleConnection(conn *Connection) {
 				conn.WriteSimpleString("PONG")
 				continue
 			case "set":
-				if len(values) != 3 {
+				if len(values) < 3 {
 					conn.WriteError(errors.New("ERR wrong number of arguments for 'set' command"))
 				} else {
-					server.mu.Lock()
-					server.kvs[values[1].String()] = values[2]
-					server.mu.Unlock()
+					server.set(values)
 					conn.WriteSimpleString("OK")
 				}
 				continue
@@ -85,13 +116,11 @@ func (server *Server) HandleConnection(conn *Connection) {
 				if len(values) != 2 {
 					conn.WriteError(errors.New("ERR wrong number of arguments for 'get' command"))
 				} else {
-					server.mu.RLock()
-					s, ok := server.kvs[values[1].String()]
-					server.mu.RUnlock()
+					s, ok := server.get(values[1].String())
 					if !ok {
 						conn.WriteNull()
 					} else {
-						conn.WriteString(s.String())
+						conn.WriteString(s.value.String())
 					}
 				}
 				continue
